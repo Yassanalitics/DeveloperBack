135d3e772ad9afe1b3b240a8fa55bf3057628319
Yassanalitics <yasminribeiro152005@gmail.com>
2025-08-27 20:33:19 -0300

    03 exercicio prático 27/08

---
+++ exercicios/exercico01-Calculadora-pratica/Calculadora.js
@@ -0,0 +1,28 @@
+function Soma(a, b){
+    return a + b
+}
+function Subtracao(a, b){
+    return a - b
+}
+function Multiplicacao(a, b){
+    return a * b
+}
+function Divisao(a, b){
+    return a / b
+}
+function aoQuadrado(a){
+    return a * a
+}
+function raizQuadrada(a){
+    return a ** 0,5 
+    //exponenciação
+}
+
+module.exports = {
+Soma,
+Subtracao,
+Divisao,
+Multiplicacao,
+aoQuadrado,
+raizQuadrada
+}
