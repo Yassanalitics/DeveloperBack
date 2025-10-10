c7cd872831735d96c81dde2d6c5324b25c20a039
Yasanalitics <yasminribeiro152005@gmail.com>
2025-10-09 21:45:25 -0300

    .env utilizado

--- pratica/07-crud-tarefas/index.js
+++ pratica/07-crud-tarefas/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://Yas_Db123:Yas_3456Db@cluster0.n9daxyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+const DB_HOST=process.env.DB_HOST
+const DB_USER=process.env.DB_USER
+const DB_PASS=process.env.DB_PASS
+const DB_NAME=process.env.DB_NAME 
+const url =`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(url)
 .then(() =>{
     console.log("conectado ao mongo db")
 })
